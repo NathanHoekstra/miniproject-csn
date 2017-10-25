8274ccf81427fa8758ccfb6faf5b3f29face1299
56KbModem <nick-snel@hotmail.com>
2017-10-25 12:20:56 +0200

    added logging capabilities

--- alarm_server.py
+++ alarm_server.py
@@ -1,5 +1,6 @@
 import socket
-from os import *
+import os
+from time import gmtime, strftime
 
 # Initialize socket
 HOST = 'localhost'
@@ -21,7 +22,7 @@ def client_connection(connect):
 		if data == b'CLIENT_HELLO\n':
 			reply = 'SERVER_ACK\n'
 		elif data == b'CLIENT_ALARM\n':
-			print('SOME ASSHOLE CAM INTO THE HOUSE!!')
+			log_event('SOME ASSHOLE CAME INTO THE HOUSE!!')
 			reply = 'SERVER_ALARM_NOTIFY\n'
 		else:
 			reply = 'SERVER_NACK\n'
@@ -31,6 +32,23 @@ def client_connection(connect):
 		connect.sendall(str.encode(reply))
 	connect.close()
 
+# Check if logfile exists, else create it
+def write_or_append():
+	if os.path.exists('/var/log/alarm.log') == True:
+		file_mode = 'a'
+	else:
+		file_mode = 'w'
+	return file_mode
+
+# Log server events
+def log_event(message):
+	file_mode = write_or_append()
+	log_file = open('/var/log/alarm.log', file_mode)
+	time_stamp = strftime("%Y-%m-%d %H:%M:%S", gmtime())
+
+	event = message + ' ' + time_stamp + '\n'
+
+	log_file.write(event)
 # Main loop
 while True:
 
